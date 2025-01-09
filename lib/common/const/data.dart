9f4ffd3c75bd523a0389e2be4cc3d8ffa2c9ed09
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2025-01-09 14:35:50 +0900

    shared_preferences 컴포넌트 구현

--- lib/common/const/data.dart
+++ lib/common/const/data.dart
@@ -1,1 +1,9 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 const IP = "http://localhost:4000";
+
+Future<SharedPreferences> prefs() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  return prefs;
+}

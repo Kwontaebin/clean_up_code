f7ff8053aa573d2dee2f47029d4cd0759e420221
kwontaebin <144872939+Kwontaebin@users.noreply.github.com>
2025-01-15 15:33:06 +0900

    서버가 연결되어있지 않을때 "서버 연결이 안 돼 있습니다" 토스트바, 로그 찍고 onData에 값 저장안하도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'clean up code!',
-      home: BackendConnection(),
+      home: SizeScreen(),
     );
   }
 }
